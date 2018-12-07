91257272feca216dbfab20101bbc88150075f56e
rsrocki01 <45692158+rsrocki01@users.noreply.github.com>
2018-12-07 08:52:09 -0600

    Add files via upload

---
+++ hmw5.py
@@ -0,0 +1,393 @@
+#3.1
+names = ['ron', 'tyler', 'dani']
+
+print(names[0])
+print(names[1])
+print(names[2])
+#3.2
+names = ['ron', 'tyler', 'dani']
+
+msg = "Hello, " + names[0].title() + "!"
+print(msg)
+
+msg = "Hello, " + names[1].title() + "!"
+print(msg)
+
+msg = "Hello, " + names[2].title() + "!"
+print(msg)
+
+#3.4
+guests = ['guido van rossum', 'jack turner', 'lynn hill']
+
+name = guests[0].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
+#3.5
+# Invite some people to dinner.
+guests = ['guido van rossum', 'jack turner', 'lynn hill']
+
+name = guests[0].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print("\nSorry, " + name + " can't make it to dinner.")
+
+# Jack can't make it! Let's invite Gary instead.
+del(guests[1])
+guests.insert(1, 'gary snyder')
+
+# Print the invitations again.
+name = guests[0].title()
+print("\n" + name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
+#3.6
+# Invite some people to dinner.
+guests = ['guido van rossum', 'jack turner', 'lynn hill']
+
+name = guests[0].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print("\nSorry, " + name + " can't make it to dinner.")
+
+# Jack can't make it! Let's invite Gary instead.
+del(guests[1])
+guests.insert(1, 'gary snyder')
+
+# Print the invitations again.
+name = guests[0].title()
+print("\n" + name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
+
+# We got a bigger table, so let's add some more people to the list.
+print("\nWe got a bigger table!")
+guests.insert(0, 'frida kahlo')
+guests.insert(2, 'reinhold messner')
+guests.append('elizabeth peratrovich')
+
+name = guests[0].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
+
+name = guests[3].title()
+print(name + ", please come to dinner.")
+
+name = guests[4].title()
+print(name + ", please come to dinner.")
+
+name = guests[5].title()
+print(name + ", please come to dinner.")
+#3.7
+# Invite some people to dinner.
+guests = ['guido van rossum', 'jack turner', 'lynn hill']
+
+name = guests[0].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print("\nSorry, " + name + " can't make it to dinner.")
+
+# Jack can't make it! Let's invite Gary instead.
+del(guests[1])
+guests.insert(1, 'gary snyder')
+
+# Print the invitations again.
+name = guests[0].title()
+print("\n" + name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
+
+# We got a bigger table, so let's add some more people to the list.
+print("\nWe got a bigger table!")
+guests.insert(0, 'frida kahlo')
+guests.insert(2, 'reinhold messner')
+guests.append('elizabeth peratrovich')
+
+name = guests[0].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
+
+name = guests[3].title()
+print(name + ", please come to dinner.")
+
+name = guests[4].title()
+print(name + ", please come to dinner.")
+
+name = guests[5].title()
+print(name + ", please come to dinner.")
+
+# Oh no, the table won't arrive on time!
+print("\nSorry, we can only invite two people to dinner.")
+
+name = guests.pop()
+print("Sorry, " + name.title() + " there's no room at the table.")
+
+name = guests.pop()
+print("Sorry, " + name.title() + " there's no room at the table.")
+
+name = guests.pop()
+print("Sorry, " + name.title() + " there's no room at the table.")
+
+name = guests.pop()
+print("Sorry, " + name.title() + " there's no room at the table.")
+
+# There should be two people left. Let's invite them.
+name = guests[0].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+# Empty out the list.
+del(guests[0])
+del(guests[0])
+
+# Prove the list is empty.
+print(guests)
+#3.8
+locations = ['himalaya', 'andes', 'tierra del fuego', 'labrador', 'guam']
+
+print("Original order:")
+print(locations)
+
+print("\nAlphabetical:")
+print(sorted(locations))
+
+print("\nOriginal order:")
+print(locations)
+
+print("\nReverse alphabetical:")
+print(sorted(locations, reverse=True))
+
+print("\nOriginal order:")
+print(locations)
+
+print("\nReversed:")
+locations.reverse()
+print(locations)
+
+print("\nOriginal order:")
+locations.reverse()
+print(locations)
+
+print("\nAlphabetical")
+locations.sort()
+print(locations)
+
+print("\nReverse alphabetical")
+locations.sort(reverse=True)
+print(locations)
+#4.1
+favorite_pizzas = ['pepperoni', 'hawaiian', 'veggie']
+
+# Print the names of all the pizzas.
+for pizza in favorite_pizzas:
+    print(pizza)
+
+print("\n")
+
+# Print a sentence about each pizza.
+for pizza in favorite_pizzas:
+    print("I really love " + pizza + " pizza!")
+
+print("\nI really love pizza!")
+#4.3
+numbers = list(range(1, 21))
+
+for number in numbers:
+    print(number)
+#4.5
+numbers = list(range(1, 1000001))
+
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+#4.7
+threes = list(range(3, 31, 3))
+
+for number in threes:
+    print(number)
+#4.8
+cubes = []
+for number in range(1, 11):
+    cube = number**3
+    cubes.append(cube)
+
+for cube in cubes:
+    print(cube)
+#4.9
+cubes = [number**3 for number in range(1,11)]
+
+for cube in cubes:
+    print(cube)
+#4.11
+favorite_pizzas = ['pepperoni', 'hawaiian', 'veggie']
+friend_pizzas = favorite_pizzas[:]
+
+favorite_pizzas.append("meat lover's")
+friend_pizzas.append('pesto')
+
+print("My favorite pizzas are:")
+for pizza in favorite_pizzas:
+    print("- " + pizza)
+
+print("\nMy friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+    print("- " + pizza)
+#4.13
+menu_items = (
+    'rockfish sandwich', 'halibut nuggets', 'smoked salmon chowder',
+    'salmon burger', 'crab cakes',
+    )
+
+print("You can choose from the following menu items:")
+for item in menu_items:
+    print("- " + item)
+
+menu_items = (
+    'rockfish sandwich', 'halibut nuggets', 'smoked salmon chowder',
+    'black cod tips', 'king crab legs',
+    )
+
+print("\nOur menu has been updated.")
+print("You can now choose from the following items:")
+for item in menu_items:
+    print("- " + item)
+#5.3
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+#5.4
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
+#5.5
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
+#5.6
+age = 17
+
+if age < 2:
+    print("You're a baby!")
+elif age < 4:
+    print("You're a toddler!")
+elif age < 13:
+    print("You're a kid!")
+elif age < 20:
+    print("You're a teenager!")
+elif age < 65:
+    print("You're an adult!")
+else:
+    print("You're an elder!")
+#5.7
+favorite_fruits = ['blueberries', 'salmonberries', 'peaches']
+
+if 'bananas' in favorite_fruits:
+    print("You really like bananas!")
+if 'apples' in favorite_fruits:
+    print("You really like apples!")
+if 'blueberries' in favorite_fruits:
+    print("You really like blueberries!")
+if 'kiwis' in favorite_fruits:
+    print("You really like kiwis!")
+if 'peaches' in favorite_fruits:
+    print("You really like peaches!")
+#5.8
+usernames = ['eric', 'willie', 'admin', 'erin', 'ever']
+
+for username in usernames:
+    if username == 'admin':
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print("Hello " + username + ", thank you for logging in again!")
+#5.9
+usernames = []
+
+if usernames:
+    for username in usernames:
+        if username == 'admin':
+            print("Hello admin, would you like to see a status report?")
+        else:
+            print("Hello " + username + ", thank you for logging in again!")
+else:
+    print("We need to find some users!")
+#5.10
+current_users = ['eric', 'willie', 'admin', 'erin', 'Ever']
+new_users = ['sarah', 'Willie', 'PHIL', 'ever', 'Iona']
+
+current_users_lower = [user.lower() for user in current_users]
+
+for new_user in new_users:
+    if new_user.lower() in current_users_lower:
+        print("Sorry " + new_user + ", that name is taken.")
+    else:
+        print("Great, " + new_user + " is still available."]
+#5.11
+numbers = list(range(1,10))
+
+for number in numbers:
+    if number == 1:
+        print("1st")
+    elif number == 2:
+        print("2nd")
+    elif number == 3:
+        print("3rd")
+    else:
+        print(str(number) + "th")
